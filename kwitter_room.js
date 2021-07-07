0b85e16f7b281f2e0317880fca603eb42a0fe7cd
Arnav-tech-06 <84869840+Arnav-tech-06@users.noreply.github.com>
2021-07-07 08:47:11 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAbdx505UMM6njyRZl0AivwKPu3Hk9YII0",
+    authDomain: "project-kwitter-4feba.firebaseapp.com",
+    databaseURL: "https://project-kwitter-4feba-default-rtdb.firebaseio.com",
+    projectId: "project-kwitter-4feba",
+    storageBucket: "project-kwitter-4feba.appspot.com",
+    messagingSenderId: "56206826139",
+    appId: "1:56206826139:web:bd8aeff3c01b6723c5443e"
+  };
